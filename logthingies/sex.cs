b2cb96befc6382fde7d9c9e9ac47915333e4c339
random <78513130+Randomhmmyes@users.noreply.github.com>
2022-08-31 02:04:41 +0200

    Initial commit

---
+++ logthingies/sex.cs
@@ -0,0 +1,24 @@
+﻿using BrokeProtocol.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _logthingies
+{
+    public class sex : Plugin
+    {
+        public sex()
+        {
+            Info = new PluginInfo("log thiny for james <3 - random", "sex");
+            Application.logMessageReceived += Application_logMessageReceived;
+        }
+
+        private void Application_logMessageReceived(string logContents, string stackTrace, LogType type)
+        {
+            System.IO.File.AppendAllText("Server2.log", String.IsNullOrWhiteSpace(stackTrace) ? $"{logContents}\n" : $"{logContents}\n{stackTrace}\n");
+        }
+    }
+}
